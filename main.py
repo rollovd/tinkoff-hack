33cd4cbfc8f73db689ce979ec9ab2d555e0d917d
gitgpns <58300712+gitgpns@users.noreply.github.com>
2020-09-11 23:13:40 +0300

    'initial'

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.api_url = "https://api.telegram.org/bot{}/".format(self.token)
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'

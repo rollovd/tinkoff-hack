ffa9eeb900e1c3ca13634eb740d37bd1a1f06ca4
gitgpns <58300712+gitgpns@users.noreply.github.com>
2020-09-12 00:37:35 +0300

    dsafdsf

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ def welcome(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = telebot.types.KeyboardButton('хай')
     item2 = telebot.types.KeyboardButton('Привет')
-
     markup.add(item1, item2)
 
-    bot.send_message(message.chat.id, reply_markup=markup)
+    bot.send_message(message.chat.id, 'Хеллоу!', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def send_message(message):
     if message.chat.type == 'private':
         if message.text == 'хай':
-            bot.send_message((message.chat.id, str(5)))
+            bot.send_message(message.chat.id, str(5))
 
         elif message.text == 'Привет':
             bot.send_message(message.chat.id, 'Гуд!')

dcee5209e3c3d755c53eb3e9d40c26b61d290091
gitgpns <58300712+gitgpns@users.noreply.github.com>
2020-09-12 00:31:48 +0300

    add code

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = telebot.types.KeyboardButton('хай')
+    item2 = telebot.types.KeyboardButton('Привет')
 
-bot.polling(none_stop=True)
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id, reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    if message.chat.type == 'private':
+        if message.text == 'хай':
+            bot.send_message((message.chat.id, str(5)))
+
+        elif message.text == 'Привет':
+            bot.send_message(message.chat.id, 'Гуд!')
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
 
